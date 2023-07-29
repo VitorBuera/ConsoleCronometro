97f9bb01721f1eedb0e95c5e373724e1730aa899
Vitor <vitor.samuel10@gmail.com>
2023-07-29 15:13:36 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿PreStart();
+
+static void PreStart()
+{
+    Console.WriteLine("Preparar");
+    Thread.Sleep(1000);
+    Console.WriteLine("Apontar");
+    Thread.Sleep(1000);
+    Console.WriteLine("Fogo");
+    Thread.Sleep(1000);
+    Stopwach();
+}
+
+static void Stopwach()
+{
+    var time = 0;
+    while (true)
+    {
+        Console.Clear();
+        Console.WriteLine("Para parar a contagem precione Ctrl+C.");
+        Console.WriteLine(time);
+        time++;
+        Thread.Sleep(1000);
+    }
+
+}
